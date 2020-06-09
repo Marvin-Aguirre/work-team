452a83299beed730750125cc19f4a50882c9d6e3
ana-marta18 <anamartasandoval01@gmail.com>
2020-06-08 20:39:17 -0600

    Se creó la clase de cola

--- Ana-San/snake.js
+++ Ana-San/snake.js
@@ -16,3 +16,36 @@ class objeto {
 		}
 	}
 }
+//SE CREO LA CLASE COLA
+class Cola extends objeto {
+	constructor(x,y){
+		super();
+		this.x = x;
+		this.y = y;
+		this.siguiente = null;
+	}
+	dibujar(ctx){
+		if(this.siguiente != null){
+			this.siguiente.dibujar(ctx);
+		}
+		ctx.fillStyle = "#0000FF";
+		ctx.fillRect(this.x, this.y, this.tamano, this.tamano);
+	}
+	setxy(x,y){
+		if(this.siguiente != null){
+			this.siguiente.setxy(this.x, this.y);
+		}
+		this.x = x;
+		this.y = y;
+	}
+	meter(){
+		if(this.siguiente == null){
+			this.siguiente = new Cola(this.x, this.y);
+		} else {
+			this.siguiente.meter();
+		}
+	}
+	verSiguiente(){
+		return this.siguiente;
+	}
+}
