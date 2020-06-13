1c3120e2dcc6273ee4e7d2d0f6059bf1b4c712d3
pris826 <portizl@miumg.edu.gt>
2020-06-12 21:04:40 -0600

    Se cambiaron los colores

--- Pri-Ort/Cambia el color.js
+++
@@ -1,8 +0,0 @@
-// funcion para cambiar el color del background
-function changeBodyBg(color){
-    document.body.style.background = color;
-}
-// funcion para cambiar el color de fondo del encabezado
-function changeHeadingBg(color){
-    document.getElementById("heading").style.background = color;
-}
